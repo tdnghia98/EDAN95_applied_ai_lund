d1024c97a14315d2c88ce7504baf98a87f640e28
Duy Nghia Tran <tdnghia98@gmail.com>
2020-12-14 22:07:50 +0100

    Initial files for lab 7

--- lab_5_nb/MNIST.py
+++ lab_5_nb/MNIST.py
@@ -9,8 +9,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-class MNISTData :
 
+class MNISTData:
     def __init__(self, dir):
 
         filelist = sorted(glob.glob(dir))
@@ -19,20 +19,27 @@ class MNISTData :
         self.samples_per_class = 500
         self.number_of_classes = 10
 
-        self.y = np.zeros(self.number_of_classes * self.samples_per_class,dtype=int)
-        for cls in range(1,self.number_of_classes):
-            self.y[(cls*500):(cls+1)*500] = cls
-
+        self.y = np.zeros(self.number_of_classes * self.samples_per_class, dtype=int)
+        for cls in range(1, self.number_of_classes):
+            self.y[(cls * 500) : (cls + 1) * 500] = cls
 
     def get_data(self):
 
-        self.train_features, self.test_features, self.train_labels, self.test_labels = train_test_split(self.x, self.y, test_size=0.3,
-                                                                                    random_state=42)
+        (
+            self.train_features,
+            self.test_features,
+            self.train_labels,
+            self.test_labels,
+        ) = train_test_split(self.x, self.y, test_size=0.3, random_state=42)
         self.train_normalised = self.train_features.reshape(3500, 400) / 255.0
         self.test_normalised = self.test_features.reshape(1500, 400) / 255.0
 
-        return self.train_normalised, self.test_normalised, self.train_labels, self.test_labels
-
+        return (
+            self.train_normalised,
+            self.test_normalised,
+            self.train_labels,
+            self.test_labels,
+        )
 
     def visualize_random(self):
 
@@ -41,23 +48,35 @@ class MNISTData :
             idxs = np.where(self.train_labels == cls)[0]
             idxs = np.random.choice(idxs, examples_per_class, replace=False)
             for i, idx in enumerate(idxs):
-                plt.subplot(examples_per_class, self.number_of_classes, i * self.number_of_classes + cls + 1)
-                plt.imshow(self.train_features[idx].astype('uint8'), cmap='gray')
-                plt.axis('off')
+                plt.subplot(
+                    examples_per_class,
+                    self.number_of_classes,
+                    i * self.number_of_classes + cls + 1,
+                )
+                plt.imshow(self.train_features[idx].astype("uint8"), cmap="gray")
+                plt.axis("off")
                 if i == 0:
                     plt.title(str(cls))
         plt.show()
 
     def visualize_wrong_class(self, pred, examples_per_class):
         for cls in range(self.number_of_classes):
-            idxs = [idx for idx, value in enumerate(self.test_labels) if((value != cls) and (pred[idx] == cls))]
-            if(len(idxs) > examples_per_class):
+            idxs = [
+                idx
+                for idx, value in enumerate(self.test_labels)
+                if ((value != cls) and (pred[idx] == cls))
+            ]
+            if len(idxs) > examples_per_class:
                 idxs = np.random.choice(idxs, examples_per_class, replace=False)
 
             for i, idx in enumerate(idxs):
-                plt.subplot(examples_per_class, self.number_of_classes, i * self.number_of_classes + cls + 1)
-                plt.imshow(self.test_features[idx].astype('uint8'), cmap='gray')
-                plt.axis('off')
+                plt.subplot(
+                    examples_per_class,
+                    self.number_of_classes,
+                    i * self.number_of_classes + cls + 1,
+                )
+                plt.imshow(self.test_features[idx].astype("uint8"), cmap="gray")
+                plt.axis("off")
                 if i == 0:
                     plt.title(str(cls))
         plt.show()
