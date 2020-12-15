b42579e1c8b1bde86133f42491041c45d213f9a9
Duy Nghia Tran <tdnghia98@gmail.com>
2020-12-15 10:51:35 +0100

    not perfect blackjack player

--- lab_7_rl/blackjack.py
+++ lab_7_rl/blackjack.py
@@ -2,8 +2,10 @@ import gym
 from gym import spaces
 from gym.utils import seeding
 
+
 def cmp(a, b):
     return int((a > b)) - int((a < b))
+
 
 # 1 = Ace, 2-10 = Number cards, Jack/Queen/King = 10
 deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -23,7 +25,7 @@ def usable_ace(hand):  # Does this hand have a usable ace?
 
 def sum_hand(hand):  # Return current hand total
     if usable_ace(hand):
-            return sum(hand) + 10
+        return sum(hand) + 10
     return sum(hand)
 
 
@@ -64,19 +66,19 @@ class BlackjackEnv(gym.Env):
     by Sutton and Barto (1998).
     https://webdocs.cs.ualberta.ca/~sutton/book/the-book.html
     """
+
     def __init__(self, natural=False):
         self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Tuple((
-            spaces.Discrete(32),
-            spaces.Discrete(11),
-            spaces.Discrete(2)))
+        self.observation_space = spaces.Tuple(
+            (spaces.Discrete(32), spaces.Discrete(11), spaces.Discrete(2))
+        )
         self._seed()
 
         # Flag to payout 1.5 on a "natural" blackjack win, like casino rules
         # Ref: http://www.bicyclecards.com/how-to-play/blackjack/
         self.natural = natural
         # Start the first game
-        self._reset()        # Number of 
+        self._reset()  # Number of
         self.nA = 2
 
     def reset(self):
@@ -90,7 +92,8 @@ class BlackjackEnv(gym.Env):
         return [seed]
 
     def _step(self, action):
-        assert self.action_space.contains(action)
+        if not self.action_space.contains(action):
+            raise ValueError(action, " not in action space")
         if action:  # hit: add a card to players hand and return
             self.player.append(draw_card(self.np_random))
             if is_bust(self.player):
